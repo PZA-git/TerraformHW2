fb38e0c581a5f6543149a34543f4fa762149c2e6
cofa <cofa@cofa.ru>
2020-05-10 18:27:57 +0300

    file_connection

--- config.tf
+++ config.tf
@@ -70,7 +70,7 @@ resource "aws_instance" "tomcat8" {
     user        = "ubuntu"
     agent       = false
     private_key = "${file("~/.ssh/my-key4.pem")}"
-    host = "Git and Maven machine"
+    host = "aws_instance.gitmvn"
    } 
  }
 }
