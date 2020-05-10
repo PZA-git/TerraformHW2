efcc307213e0c6acacb9fbb2052f3420f83b7f76
cofa <cofa@cofa.ru>
2020-05-10 15:51:22 +0300

    init

---
+++ config.tf
@@ -0,0 +1,22 @@
+  
+provider "aws" {
+ region = "eu-central-1"
+}
+
+resource "aws_instance" "gitmvn" {
+ ami = "ami-0e342d72b12109f91"
+ instance_type = "t2.micro"
+ key_name = "my-key4"
+ security_groups = ["launch-wizard-2"]
+ connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    agent       = false
+    private_key = "${file("~/.ssh/my-key4.pem")}"
+  } 
+
+ provisioner "file"{
+   source      = "https://github.com/PZA-git/boxfuse3.git"
+   destination = "myapp"
+ }
+}
