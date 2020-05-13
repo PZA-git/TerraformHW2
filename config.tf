f46b3b1e2033b2694caf7a5a74b783cb6af3da8d
cofa <cofa@cofa.ru>
2020-05-13 22:04:43 +0300

    add pull artifact

--- config.tf
+++ config.tf
@@ -34,9 +34,9 @@ resource "aws_instance" "gitmvn" {
          "sudo apt update && sudo apt install -y git && sudo apt install -y maven && sudo apt install -y awscli",
          "git clone https://github.com/PZA-git/boxfuse3.git myapp",
          "cd myapp && mvn package", 
-         "sudo mkdir ~/.aws",
-         "sudo mv ~/credentials ~/.aws/credentials",         
-         "sudo aws s3 cp ~/myapp/target/hello-1.0.war s3://terrahw",
+         "mkdir ~/.aws",
+         "mv ~/credentials ~/.aws/credentials",         
+         "aws s3 cp ~/myapp/target/hello-1.0.war s3://terrahw",
          ]
  }
 }
